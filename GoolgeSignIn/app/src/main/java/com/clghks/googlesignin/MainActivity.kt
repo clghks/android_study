376e5b8b2c049a0f9a0ecbab8b5c4fa71bdb7f12
Choi Cihwan <clghks@gmail.com>
2018-05-13 16:35:40 +0900

    google game 로그인

--- GoolgeSignIn/app/src/main/java/com/clghks/googlesignin/MainActivity.kt
+++ GoolgeSignIn/app/src/main/java/com/clghks/googlesignin/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.clghks.googlesignin
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -24,12 +24,11 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
                 .requestEmail()
                 .build()
 
-        signInClient = GoogleSignIn.getClient(this, gso)
+        signInClient = GoogleSignIn.getClient(this, googleSignInOptions)
 
         sign_in_button.setOnClickListener {
             signIn()
@@ -52,14 +51,16 @@ class MainActivity : BaseActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode === RC_SIGN_IN) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                val account = task.getResult(ApiException::class.java)
-                updateUI(account)
-            } catch (e: ApiException) {
-                // Google Sign In failed, update UI appropriately
-                Log.w(TAG, "Google sign in failed", e)
-                updateUI(null)
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (result.isSuccess){
+                val signedInAccount = result.signInAccount
+                updateUI(signedInAccount)
+            } else {
+                var message = result.status.statusMessage
+                if (message == null || message.isEmpty()) {
+                    message = getString(R.string.signin_other_error)
+                }
+                AlertDialog.Builder(this).setMessage(message).setNeutralButton(android.R.string.ok, null).show()
             }
         }
     }
