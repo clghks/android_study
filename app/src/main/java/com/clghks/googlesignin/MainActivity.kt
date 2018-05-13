c367eac51594ea791c71c777ebecd8f3c1644021
Choi Cihwan <clghks@gmail.com>
2018-05-13 15:49:07 +0900

    firebase 삭제

--- app/src/main/java/com/clghks/googlesignin/MainActivity.kt
+++ app/src/main/java/com/clghks/googlesignin/MainActivity.kt
@@ -4,15 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Toast
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -23,7 +19,6 @@ class MainActivity : BaseActivity() {
     }
 
     private var mGoogleSignInClient: GoogleSignInClient? = null
-    private var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +30,6 @@ class MainActivity : BaseActivity() {
                 .build()
 
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
-        mAuth = FirebaseAuth.getInstance()
 
         sign_in_button.setOnClickListener {
             signIn()
@@ -49,8 +43,7 @@ class MainActivity : BaseActivity() {
     override fun onStart() {
         super.onStart()
 
-        val currentUser = mAuth?.currentUser
-        updateUI(currentUser)
+        signInSilently()
     }
 
     private fun signIn() {
@@ -66,20 +59,30 @@ class MainActivity : BaseActivity() {
             try {
                 // Google Sign In was successful, authenticate with Firebase
                 val account = task.getResult(ApiException::class.java)
-                firebaseAuthWithGoogle(account)
+                onConnected(account)
             } catch (e: ApiException) {
                 // Google Sign In failed, update UI appropriately
                 Log.w(TAG, "Google sign in failed", e)
-                updateUI(null)
+                onDisconnected()
             }
         }
     }
 
-    private fun updateUI(user: FirebaseUser?) {
+    private fun onConnected(googleSignInAccount: GoogleSignInAccount) {
+        Log.d(TAG, "onConnected(): connected to Google APIs")
+        updateUI(googleSignInAccount)
+    }
+
+    private fun onDisconnected() {
+        Log.d(TAG, "onDisconnected()")
+        updateUI(null)
+    }
+
+    private fun updateUI(user: GoogleSignInAccount?) {
         hideProgressDialog()
         if (user != null) {
             login_user_email.text = getString(R.string.google_status_fmt, user.email)
-            login_user_id.text = getString(R.string.firebase_status_fmt, user.uid)
+            login_user_id.text = ""//getString(R.string.firebase_status_fmt, user.uid)
 
             sign_in_button.visibility = View.GONE
             sign_out_button.visibility = View.VISIBLE
@@ -93,33 +96,22 @@ class MainActivity : BaseActivity() {
     }
 
     private fun signOut() {
-        // Firebase sign out
-        mAuth?.signOut()
-
         // Google sign out
         mGoogleSignInClient?.signOut()?.addOnCompleteListener(this) { updateUI(null) }
     }
 
 
-    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
-        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.id!!)
+    private fun signInSilently() {
         showProgressDialog()
-
-        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
-        mAuth?.signInWithCredential(credential)
-                ?.addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "signInWithCredential:success")
-                        val user = mAuth?.currentUser
-                        updateUI(user)
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "signInWithCredential:failure", task.exception)
-                        Toast.makeText(applicationContext, "Authentication Failed.", Toast.LENGTH_SHORT).show()
-                        updateUI(null)
-                    }
-                    hideProgressDialog()
-                }
+        mGoogleSignInClient?.silentSignIn()?.addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                Log.d(TAG, "signInSilently(): success")
+                onConnected(task.result)
+            } else {
+                Log.d(TAG, "signInSilently(): failure", task.exception)
+                onDisconnected()
+            }
+            hideProgressDialog()
+        }
     }
 }
